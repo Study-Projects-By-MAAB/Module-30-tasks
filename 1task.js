2332beaadfe9ca58677b167be1b6ed2c4122fe20
maab-fw <maabfw@proton.me>
2024-02-22 13:18:00 +0600

    1st done

---
+++ 1task.js
@@ -0,0 +1,5 @@
+// 1) Write an arrow function that will take 3 parameters, will multiply the parameters and will return the result.
+
+const arrow = (num1, num2, num3) => num1 * num2 * num3
+
+console.log(arrow(3,4,5));
